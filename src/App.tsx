d6ed3e57fb6e1a97a6784b608926b4a254e1728b
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-06-02 15:50:39 +0200

    feat: add Todo interface and dummy data for initial todos

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ function App() {
 
 
   return (
-    <>
-      <main className="py-10 bg-red h-screen">
-        <h1>Your Todos</h1>
+ 
+      <main className="py-10 h-screen">
+      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+
       </main>
-    </>
+  
   )
 }
 
---
+++ src/data/todos.ts
@@ -0,0 +1,9 @@
+import type { Todo } from "../types/todo";
+
+export const dummyData:Todo[] = [
+  { id: 1, title: "Buy groceries", completed: false },
+  { id: 2, title: "Finish React project", completed: true },
+  { id: 3, title: "Walk the dog", completed: false },
+  { id: 4, title: "Read a book", completed: true },
+  { id: 5, title: "Call mom", completed: false },
+];
