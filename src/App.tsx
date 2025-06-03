faa8641003703b7a1b1f2f137f9d00d66a65434d
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-06-03 10:16:08 +0200

    refactor: enhance TodoItem component to handle completion state and improve App structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import TodoItem from "./components/TodoItem"
-import { dummyData } from "./data/todos"
+import TodoItem from "./components/TodoItem";
+import { dummyData } from "./data/todos";
 
 function App() {
 
+    const [todos, setTodos] = useState(dummyData);
 
-  return (
- 
-      <main className="py-10 h-screen space-y">
-      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
-        <div className="max-w-lg mx-auto bg-slate-100 p-5 rounded-md">
-          <div className="space-y-2">
-            {dummyData.map(todo =>(
-              <TodoItem key={todo.id} todo={todo}/>
-            ))}
-          </div>
-        </div>
-
-   
-
-      </main>
-  
-  )
+ function setTodoCompleted(id: number, completed: boolean) {
+  setTodos((prevTodos) =>
+    prevTodos.map((todo) =>
+      todo.id === id ? { ...todo, completed } : todo
+    )
+  );
 }
 
-export default App
+  return (
+    <main className="py-10 h-screen space-y-5">
+      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+      <div className="max-w-lg mx-auto bg-slate-100 p-5 rounded-md">
+        <div className="space-y-2">
+          {dummyData.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onCompletedChange={setTodoCompleted}
+            />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,25 +1,21 @@
-import type { Todo } from '../types/todo';
-
-
+import type { Todo } from "../types/todo";
 
 interface TodoItemProps {
-todo: Todo;
+  todo: Todo;
+  onCompletedChange: (id: number, completed: boolean) => void;
 }
 
-const TodoItem = ({todo}: TodoItemProps) => {
+const TodoItem = ({ todo, onCompletedChange }: TodoItemProps) => {
   return (
     <div>
-     <label className='flex items-center gap-2 cursor-pointer border rounded-md p-2 border-gray-400 bg-white hover:bg-gray-100 transition-colors'>
-        <input
-        type='checkbox'
-        className='scale-125 ' />
-        <span className={todo.completed ? 'line-through text-gray-500' : ''}>
-            {todo.title}
+      <label className="flex items-center gap-2 cursor-pointer border rounded-md p-2 border-gray-400 bg-white hover:bg-gray-100 transition-colors">
+        <input type="checkbox" onChange={(e) => onCompletedChange(todo.id, e.target.checked) } checked={todo.completed} className="scale-125 " />
+        <span className={todo.completed ? "line-through text-gray-500" : ""}>
+          {todo.title}
         </span>
-      
-     </label>
+      </label>
     </div>
-  )
-}
+  );
+};
 
-export default TodoItem
+export default TodoItem;
